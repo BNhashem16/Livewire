0491c7baaac2149e1246048a693ce815813cc5bb
BNHashem <a.hashem@thetailorsdev.com>
2021-03-16 02:02:13 +0200

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('frontend.index');
+    }
+
+    public function create()
+    {
+        return view('frontend.create');
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+    }
+
+    public function show($id)
+    {
+        return view('frontend.show');
+    }
+
+    public function edit($id)
+    {
+        return view('frontend.edit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        return view('frontend.edit');
+    }
 }
